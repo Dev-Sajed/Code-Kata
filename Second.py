9999ada6157366df424d6479397895833c2b0e44
Dev-Sajed <42357317+Dev-Sajed@users.noreply.github.com>
2018-08-31 15:52:38 +0530

    Update Second.py

--- Second.py
+++ Second.py
@@ -1,6 +1,9 @@
 #To Check Whether The Given Number Is Odd OR Even
 a=int(input("Enter a Number"))
-if(a%2==0):
-  print("Even")
+if(a>0):
+  if(a%2==0):
+    print("Even")
+  else:
+    print("Odd")
 else:
-  print("Odd")
+  print("Invalid")
