279988e7cfcec7ec29beb330cc1e4bfa3880add3
Mohamed Sajed <42357317+Dev-Sajed@users.noreply.github.com>
2019-02-07 10:27:46 +0530

    Update third.py

--- third.py
+++ third.py
@@ -1,4 +1,4 @@
-ch = input("Enter an Alphabet: ")
+ch = input("")
 alphabets=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 if ch in alphabets:
   vowel=['a','e','i','o','u','A','E','I','O','U']
