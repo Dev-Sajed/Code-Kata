c1a0486d9f84e40457708f8a99ae8726489f5ebc
Mohamed Sajed <42357317+Dev-Sajed@users.noreply.github.com>
2019-02-07 15:10:03 +0530

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -1,12 +1,12 @@
-n=input()
-n=n.split()
+n=input().split()
 a=int(n[0])
 b=int(n[1])
 c=int(n[2])
-if (a>c) and (a>b):
-  largest=a
-  if (b>a) and (b>c):
-    largest=b
+if(a>b and a>c):
+  print(a)
+elif(b>a and b>c):
+  print(b)
+elif(c>a and c>b):
+  print(c)
 else:
-  largest=c
-print(largest)
+  print("Invalid")
